2bbd62fbeb1e95aa87d59477806249fffb9c3f36
unity-gdc <george@codingpa.com>
2023-12-29 21:50:09 -0500

    added export

--- pf-rewards.py
+++ pf-rewards.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import sys
+import pandas as pd
 
 def get_credentials():
     # Read the credentials and IP from the file
@@ -31,8 +32,26 @@ def connect_to_db(config):
         print(f"Failed to connect: {err}")
         sys.exit(1)  # Exit the program
 
+def fetch_data_to_dataframe(cursor, query):
+    print("Executing SQL Statment...")
+    # Execute the given SQL query
+    cursor.execute(query)
+
+    # Fetch all results
+    result = cursor.fetchall()
+
+    # Convert to DataFrame
+    columns = [i[0] for i in cursor.description]  # this gets the column names
+    df = pd.DataFrame(result, columns=columns)
+
+    return df
+
+
 def main():
     print("Starting Script")
+
+    # Set the filename variable
+    excel_file_name = "./export/pathfinder_data.xlsx"
 
     creds = get_credentials()
 
@@ -41,6 +60,21 @@ def main():
     cursor = db_connection.cursor()
 
     #Do things here
+    query = """
+        SELECT *
+        FROM activity_log
+        LEFT JOIN `character` ON activity_log.characterId = `character`.id
+        LEFT JOIN alliance ON character.allianceId = alliance.id;
+    """
+
+    df = fetch_data_to_dataframe(cursor, query)
+    
+    # Exporting to Excel with 'All Data' as the sheet name
+    print("Exporting All Data")
+    df.to_excel(excel_file_name, sheet_name='All Data', index=False)
+    print(f"Data exported to {excel_file_name} in the sheet named 'All Data'.")
+
+
 
     # Close cursor and connection
     cursor.close()
