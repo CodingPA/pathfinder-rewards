d46d41b4d5663ee4e6f6c38737bbe6e0de77a435
unity-gdc <george@codingpa.com>
2023-12-29 21:29:11 -0500

    Update pf-rewards.py

--- pf-rewards.py
+++ pf-rewards.py
@@ -1,36 +1,50 @@
 import mysql.connector
+import sys
 
-# Read the credentials and IP from the file
-with open('cred.txt', 'r') as file:
-    username = file.readline().strip()  # Read the first line for username
-    password = file.readline().strip()  # Read the second line for password
-    db_ip = file.readline().strip()     # Read the third line for server IP
+def get_credentials():
+    # Read the credentials and IP from the file
+    with open('cred.txt', 'r') as file:
+        username = file.readline().strip()  # Read the first line for username
+        password = file.readline().strip()  # Read the second line for password
+        db_ip = file.readline().strip()     # Read the third line for server IP
 
-# Connection configuration
-config = {
-    'user': username,
-    'password': password,
-    'host': db_ip,  
-    'port': 3306,
-    'database': 'mkuga_pathfinder',
-    'raise_on_warnings': True
-}
+    # Connection configuration
+    config = {
+        'user': username,
+        'password': password,
+        'host': db_ip,  
+        'port': 3306,
+        'database': 'mkuga_pathfinder',
+        'raise_on_warnings': True
+    }
 
-try:
-    # Establishing the connection to the database
-    cnx = mysql.connector.connect(**config)
-    print("Successfully connected to the database!")
+    return config
 
-    # You can now use cnx to interact with the database
-    cursor = cnx.cursor()
-    cursor.execute("SHOW TABLES;")
-    for table in cursor:
-        print(table)
+def connect_to_db(config):
+    try:
+        # Establishing the connection to the database
+        cnx = mysql.connector.connect(**config)
+        print("Successfully connected to the database!")
+        return cnx
 
-    # close the connection when done
-    cnx.close()
+    except mysql.connector.Error as err:
+        print(f"Failed to connect: {err}")
+        sys.exit(1)  # Exit the program
 
-except mysql.connector.Error as err:
-    print(f"Failed to connect: {err}")
+def main():
+    print("Starting Script")
 
-# Expand this script to interact with the database, perform queries, and handle results.
+    creds = get_credentials()
+
+    db_connection = connect_to_db(creds)
+
+    cursor = db_connection.cursor()
+
+    #Do things here
+
+    # Close cursor and connection
+    cursor.close()
+    db_connection.close()
+
+if __name__=="__main__":
+    main()
